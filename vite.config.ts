df1713bf7cd339ec02144ac61d9af934c737b3a9
argonmining <ashton@onargon.com>
2024-11-17 12:36:20 -0800

    env loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: 'VITE_'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log('Loading environment variables:', env);
+  
+  return {
+    plugins: [react()],
+    envPrefix: 'VITE_'
+  };
 }); 

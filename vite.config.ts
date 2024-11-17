43b94262dd22b2f1553a23d525f770064520fa77
argonmining <ashton@onargon.com>
2024-11-17 13:13:29 -0800

    vite builds

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  console.log('Loading environment variables:', env);
+  console.log('Loaded environment variables:', env);
   
   return {
     plugins: [react()],
